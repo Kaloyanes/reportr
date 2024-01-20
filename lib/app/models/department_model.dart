045051da8d4d93891d304e494e538dc38014af84
Valery-a <ValeryKulchicki@gmail.com>
2024-01-20 21:56:50 +0200

    added department model

---
+++ lib/app/models/department_model.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Department {
+  String id;
+  String type;
+  String address;
+
+  Department({
+    required this.id,
+    required this.type,
+    required this.address,
+  });
+
+}
