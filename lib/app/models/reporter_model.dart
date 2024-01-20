d3f3e5cc5704d810f354374cb5a32394473b88ce
Valery-a <ValeryKulchicki@gmail.com>
2024-01-20 22:00:51 +0200

    nvm

--- lib/app/models/reporter_model.dart
+++ lib/app/models/reporter_model.dart
@@ -5,13 +5,57 @@ class Reporter {
   String id;
   String name;
   String photoUrl;
-  String departmendId;
 
   Reporter({
     required this.id,
     required this.name,
     required this.photoUrl,
-    required this.departmendId,
   });
 
+  Reporter copyWith({
+    String? id,
+    String? name,
+    String? photoUrl,
+  }) {
+    return Reporter(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      photoUrl: photoUrl ?? this.photoUrl,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'photoUrl': photoUrl,
+    };
+  }
+
+  factory Reporter.fromMap(Map<String, dynamic> map, String id) {
+    return Reporter(
+      id: id,
+      name: map['name'] as String,
+      photoUrl: map['photoUrl'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Reporter.fromJson(String source, id) => Reporter.fromMap(json.decode(source) as Map<String, dynamic>, id);
+
+  @override
+  String toString() => 'Reporter(id: $id, name: $name, photoUrl: $photoUrl)';
+
+  @override
+  bool operator ==(covariant Reporter other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.name == name && other.photoUrl == photoUrl;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode ^ photoUrl.hashCode;
+
+  factory Reporter.anon() => Reporter(id: "anon", name: "Анонимен", photoUrl: "");
 }
